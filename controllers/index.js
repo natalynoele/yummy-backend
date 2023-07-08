6d0a8175323b43b0ddb13f0df7507ba3748574a8
AnastasiiaBakumenko <bakumenkoanastasia2@gmail.com>
2023-07-08 08:56:11 +0200

    Зробила окремо кожну функцію і рееспорт

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+const getById = require('./recipes/getById')
+const getCategoryList = require('./recipes/getCategoryList')
+
+module.exports={
+    getById,
+    getCategoryList,
+}
--- routes/api/recipeRouter.js
+++ routes/api/recipeRouter.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const recipeControllers = require("../../controllers/recipes/recipeControllers")
+const recipeControllers = require("../../controllers/index")
 
-// router.get("/:id", recipeControllers.getById);
+
 
 router.get("/category-list", recipeControllers.getCategoryList)
+
+router.get("/:id", recipeControllers.getById);
 
 module.exports = router
