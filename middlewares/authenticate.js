39c8424544c80befeae9daeb1024c885e9c814ac
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2023-07-08 16:20:47 +0300

    add middleware-auth and userUpdateSubscription

---
+++ middlewares/authenticate.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+
+const { HttpError } = require("../helpers");
+
+const User = require("../models/users");
+
+const { SECRET_KEY } = process.env;
+
+const authenticate = async (req, res, next) => {
+    const { authorization = "" } = req.headers;
+
+    const [bearer, token] = authorization.split(" ");
+
+    if (bearer !== "Bearer") {
+        next(HttpError(401));
+    } else {
+        try {
+            const { id } = jwt.verify(token, SECRET_KEY);
+            
+            const user = await User.findById(id);
+            if (!user || !user.token) {
+                next(HttpError(401, 'Not authorized'));
+            }
+            req.user = user;
+            next();
+            
+        } catch (error) {
+            next(HttpError(401, error.message));
+        }
+    }
+};
+
+module.exports = authenticate;
