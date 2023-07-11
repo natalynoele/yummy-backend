ce28773d33bfd1e26d93e125dddad12d4830f5f9
Macbook <natalynoele@gmail.com>
2023-07-11 23:52:19 +0300

    swagger recipes docs

--- controllers/ingredients/getAll.js
+++ controllers/ingredients/getAll.js
@@ -1,9 +1,7 @@
 const { Ingredients } = require("../../models");
 
 const getAllIngredients = async (req, res) => {
-  console.log("ingredients");
   const result = await Ingredients.find();
-  console.log(result);
   res.json(result);
 };
 
