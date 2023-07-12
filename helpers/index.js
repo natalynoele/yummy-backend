844751d44e19d6ce113fa837044d899ac1a64f0d
Yaroslav <110727630+BaristaYaroslv@users.noreply.github.com>
2023-07-12 21:42:34 +0300

    fix

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,8 @@
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
-
+const sendEmail = require("./sendEmail")
 module.exports = {
   HttpError,
   ctrlWrapper,
+  sendEmail
 };
