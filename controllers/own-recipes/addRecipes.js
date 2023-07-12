4471627f750f7648ed0ddcfe0afb3265a1fb62a6
Macbook <natalynoele@gmail.com>
2023-07-12 14:28:45 +0300

    added: owner to recipe schema

--- controllers/own-recipes/addRecipes.js
+++ controllers/own-recipes/addRecipes.js
@@ -1,21 +1,22 @@
-const {Recipe} = require("../../models");
+const { Recipe } = require("../../models");
 
-const addRecipes = async(req, res) =>{
-    // const { _id: owner } = req.user;
+const addRecipes = async (req, res) => {
+  console.log(req.user);
+  const { _id: owner } = req.user;
 
-    let recipePreview;
-  
-    if (req.file) {
-        recipePreview = req.file.path;
-    } else {
-        recipePreview = 'https://res.cloudinary.com/dvmiapyqk/image/upload/v1688894039/1_jyhhh3.png';
-    }
-  
-    const recipeData = { ...req.body, imageURL: recipePreview };
-  
-    const newRecipe = await Recipe.create({ ...recipeData});
-    res.status(201).json(newRecipe);
-}
+  let recipePreview;
+
+  if (req.file) {
+    recipePreview = req.file.path;
+  } else {
+    recipePreview =
+      "https://res.cloudinary.com/dvmiapyqk/image/upload/v1688894039/1_jyhhh3.png";
+  }
+
+  const recipeData = { ...req.body, imageURL: recipePreview, owner };
+
+  const newRecipe = await Recipe.create({ ...recipeData });
+  res.status(201).json(newRecipe);
+};
 
 module.exports = addRecipes;
-
--- models/recipes/recipes.js
+++ models/recipes/recipes.js
@@ -1,45 +1,51 @@
 const { Schema, model } = require("mongoose");
 
-const recipesSchema = new Schema({
-  title: {
-    type: String,
-    require: true, 
+const recipesSchema = new Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+    },
+    category: {
+      type: String,
+      require: true,
+    },
+    area: {
+      type: String,
+    },
+    instructions: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    preview: {
+      type: String,
+      require: true,
+    },
+    time: {
+      type: String,
+      require: true,
+    },
+    youtube: {
+      type: String,
+    },
+    tags: {
+      type: Array,
+    },
+    ingredients: {
+      type: Array,
+      require: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+    },
   },
-  category: {
-    type: String,
-    require: true, 
-  },
-  area: {
-    type: String, 
-  },
-  instructions: {
-    type: String,
-    require: true, 
-  },
-  description: {
-    type: String,
-    require: true, 
-  },
-  preview: {
-    type: String,
-    require: true,
-  },
-  time: {
-    type: String,
-    require: true,
-  },
-  youtube: {
-    type: String,
-  },
-  tags: {
-    type: Array,
-   
-  },
-  ingredients: {
-    type: Array,
-    require: true,
-  },
-}, {versionKey: false,});
+  { versionKey: false }
+);
 
 const Recipe = model("recipes", recipesSchema);
 module.exports = { Recipe };
