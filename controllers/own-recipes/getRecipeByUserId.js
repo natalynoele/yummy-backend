3c20cb905f5264533d6e5edc83842710c8a7722b
Macbook <natalynoele@gmail.com>
2023-07-17 09:49:54 +0300

    fix: get own recipes controller

--- controllers/own-recipes/getRecipeByUserId.js
+++ controllers/own-recipes/getRecipeByUserId.js
@@ -2,9 +2,10 @@ const { HttpError } = require("../../helpers");
 const { Recipe } = require("../../models");
 
 const getRecipeByUserId = async (req, res) => {
-  console.log(req.user, "ownrec");
   const { _id: owner } = req.user;
-  const result = await Recipe.find(owner);
+
+  const result = await Recipe.find({ owner });
+
   if (!result) {
     throw new HttpError(404, `Recipe not found`);
   }
