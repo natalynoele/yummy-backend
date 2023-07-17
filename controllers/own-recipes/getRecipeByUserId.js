2b0629945e06a143755c8832001a6c1e7fc86dd9
Macbook <natalynoele@gmail.com>
2023-07-17 09:31:55 +0300

    added: authenticate to ownrecipes endpoints

--- controllers/own-recipes/getRecipeByUserId.js
+++ controllers/own-recipes/getRecipeByUserId.js
@@ -1,13 +1,14 @@
 const { HttpError } = require("../../helpers");
 const { Recipe } = require("../../models");
 
-const getRecipeByUserId = async(req, res)=>{
-    const { _id: owner } = req.user;
-    const result = await Recipe.find(owner);
-    if (!result) {
-        throw new HttpError(404, `Recipe not found`)
-      }
-      res.status(200).json(result);
-}
+const getRecipeByUserId = async (req, res) => {
+  console.log(req.user, "ownrec");
+  const { _id: owner } = req.user;
+  const result = await Recipe.find(owner);
+  if (!result) {
+    throw new HttpError(404, `Recipe not found`);
+  }
+  res.status(200).json(result);
+};
 
 module.exports = getRecipeByUserId;
