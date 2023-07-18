211f2b830630b7620b2be0d123efad0a93be7be3
Macbook <natalynoele@gmail.com>
2023-07-18 20:16:36 +0300

    fix: search by ingredient

--- controllers/recipes/searchByIngredients.js
+++ controllers/recipes/searchByIngredients.js
@@ -2,10 +2,13 @@ const { RecipeService } = require("../../services");
 
 const searchByIngredients = async (req, res) => {
   const { ingredients } = req.query;
+
   if (ingredients === "") {
-    return res.status(404).json({ message: "Not found ingredients" });
+    return;
   }
+
   const result = await RecipeService.searchByIngredients(ingredients);
+
   res.json(result);
 };
 
